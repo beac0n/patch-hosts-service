e4162b66b78c6095c4a885b235c1a18aaa361594
beac0n <mcschempp@gmx.de>
2020-01-05 19:24:07 +0100

    refactor

--- src/handlers/pubsub/consumer.go
+++ src/handlers/pubsub/consumer.go
@@ -3,27 +3,22 @@ package pubsub
 import (
 	"../../utils"
 	"net/http"
-	"strconv"
 )
 
 func (requestHandler *RequestHandler) consume(request *http.Request, responseWriter http.ResponseWriter, dataChannel chan *[]byte, comChannel chan struct{}, persist bool) {
 	comChannel <- struct{}{}
 
-	if persist {
-		requestHandler.consumePersist(dataChannel, comChannel, responseWriter, request)
-	} else {
-		requestHandler.consumeNormal(dataChannel, comChannel, responseWriter, request)
-	}
-}
-
-func (requestHandler *RequestHandler) consumePersist(dataChannel chan *[]byte, comChannel chan struct{}, responseWriter http.ResponseWriter, request *http.Request) {
 	for {
 		select {
 		case bytes := <-dataChannel:
-			comChannel <- struct{}{}
+			if persist {
+				comChannel <- struct{}{}
+			}
+
 			_, err := responseWriter.Write(*bytes)
 			responseWriter.(http.Flusher).Flush()
-			if utils.LogError(err, request) {
+
+			if utils.LogError(err, request) || !persist {
 				return
 			}
 
@@ -31,17 +26,5 @@ func (requestHandler *RequestHandler) consumePersist(dataChannel chan *[]byte, c
 			<-comChannel
 			return
 		}
-	}
-}
-
-func (requestHandler *RequestHandler) consumeNormal(dataChannel chan *[]byte, comChannel chan struct{}, responseWriter http.ResponseWriter, request *http.Request) {
-	select {
-	case bytes := <-dataChannel:
-		responseWriter.Header().Set("Content-Length", strconv.Itoa(len(*bytes)))
-		_, err := responseWriter.Write(*bytes)
-		utils.LogError(err, request)
-
-	case <-request.Context().Done():
-		<-comChannel
 	}
 }
